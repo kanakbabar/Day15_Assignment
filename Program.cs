231b70447324eff4cc1d4fb98e56b2e706f080d5
kanakbabar <kanakbabar999@gamil.com>
2021-10-11 13:10:21 +0530

    [kanak]Add : Binary Search Tree Problem

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Binary Search Tree\n");
+
+            //#region UC1
+
+            //UC1<int> binarySearch = new UC1<int>(56);
+
+            //binarySearch.Insert(30);
+
+            //binarySearch.Insert(70);
+
+            //binarySearch.Display();
+
+            //#endregion UC1
+
+            //#region UC2
+
+            //UC2<int> binarysearch = new UC2<int>(56);
+
+            //binarysearch.Insert(30);
+            //binarysearch.Insert(22);
+            //binarysearch.Insert(40);
+            //binarysearch.Insert(11);
+            //binarysearch.Insert(3);
+            //binarysearch.Insert(16);
+            //binarysearch.Insert(70);
+            //binarysearch.Insert(60);
+            //binarysearch.Insert(95);
+            //binarysearch.Insert(65);
+            //binarysearch.Insert(63);
+            //binarysearch.Insert(67);
+
+            //binarysearch.Display();
+
+            //#endregion UC2
+
+
+            //#region UC3
+
+            //UC3<int> binarysearch = new UC3<int>(56);
+
+            //binarysearch.Insert(30);
+            //binarysearch.Insert(22);
+            //binarysearch.Insert(40);
+            //binarysearch.Insert(11);
+            //binarysearch.Insert(3);
+            //binarysearch.Insert(16);
+            //binarysearch.Insert(70);
+            //binarysearch.Insert(63);
+            //binarysearch.Insert(60);
+            //binarysearch.Insert(95);
+            //binarysearch.Insert(65);
+            //binarysearch.Insert(67);
+
+            //binarysearch.Display();
+
+            //bool result = binarysearch.Search(63, binarysearch);
+            //Console.WriteLine(result);
+
+            //#endregion UC3
+        }
+    }
+}
